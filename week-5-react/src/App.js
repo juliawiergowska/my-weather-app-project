0843e165c54f1315888fd05426dc03af122a52e7
jwiergowska <julia.wiergowska@gmail.com>
2021-12-12 18:08:13 +0100

    update

--- week-5-react/src/App.js
+++ week-5-react/src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import React from "react";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <h1>Weather App </h1>
+     <button className="btn btn-primary">Hello</button>
+
+     <footer>
+          This project was coded by{" "}
+          
+            Julia Wiergowska
+        
+          and is{" "}
+          <a
+            href="https://github.com/jwiergowska/weather-4thproject"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://suspicious-beaver-111c4d.netlify.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+     
+     </div>
   );
 }
 
